3769173a87c8346af844ef32930f8759d98df3fe
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2019-04-25 17:15:31 +0100

    added searchbar and clickable boxes

--- src/screens/PastQuestions.js
+++ src/screens/PastQuestions.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
-import { View, TextInput, Text, StyleSheet } from 'react-native'
+import { View, TextInput, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 
 
 class PastQuestions extends Component {
     state = {
-        searchInput: ""
+        searchInput: "",
+        PastQuestions: {
+            utme: "UTME",
+            waec: "WAEC",
+            unilagPostutme: "UNILAG POSTUTME",
+            unilorinPostutme: "UNILORIN POSTUTME",
+            unibenPostutme: "UNIBEN POSTUME",
+        }
     }
 
     render() {
+
         return (
             <View style={styles.container}>
                 <View style={styles.searchBarView}>
-                    <TextInput onChangeText={this.searchChange} placeholder="search" style={styles.searchBar} />
+                    <TextInput style={styles.searchBarInput} placeholder="Search question type"></TextInput>
                 </View>
-                <View>
 
+                <View style={styles.typeView}>
+                    <ScrollView>
+                        <View style={styles.typeView}>
+                            <TouchableOpacity style={styles.type}><Text>UTME</Text></TouchableOpacity>
+
+                            <TouchableOpacity style={styles.type}><Text>WAEC</Text></TouchableOpacity>
+                        </View>
+                        <View style={styles.typeView}>
+                            <TouchableOpacity style={styles.type}><Text>UTME</Text></TouchableOpacity>
+
+                            <TouchableOpacity style={styles.type}><Text>WAEC</Text></TouchableOpacity>
+                        </View>
+                        <View style={styles.typeView}>
+                            <TouchableOpacity style={styles.type}><Text>UTME</Text></TouchableOpacity>
+
+                            <TouchableOpacity style={styles.type}><Text>WAEC</Text></TouchableOpacity>
+                        </View>
+
+                    </ScrollView>
                 </View>
+
             </View>
         )
     }
@@ -24,11 +52,37 @@ class PastQuestions extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: "center"
     },
     searchBarView: {
-        width: "95%",
+        justifyContent: "center",
+        width: "90%",
+        height: 55,
+        borderRadius: 10,
+        elevation: 3,
+        paddingLeft: wp("8%"),
+        marginTop: hp('5%')
 
+    },
+    searchBarInput: {
+        fontSize: 20,
+        paddingTop: 12,
+        color: "#000000"
+    },
+    typeView: {
+        flexDirection: "row",
+        padding: 10,
+        justifyContent: "space-evenly",
+    },
+    typeList: {
+        justifyContent: "center"
+    },
+    type: {
+        width: 150,
+        height: 150,
+        backgroundColor: "blue",
+        alignItems: "center"
     }
 })
 
