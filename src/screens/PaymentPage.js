70e3a403e2c8081caaf650c2e2e99d40febb55e4
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2019-05-13 13:40:57 +0100

    added react-native-htmlview as an alternative

--- src/screens/PaymentPage.js
+++ src/screens/PaymentPage.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions, } from 'react-native'
 import HTML from 'react-native-render-html';
 
 const htmlContent = `
-    <h1>This HTML snippet is now rendered with native components !</h1>
-    <h2>Enjoy a webview-free and blazing fast application</h2>
-    <img src="https://i.imgur.com/dHLmxfO.jpg?2" />
-    <em style="textAlign: center;">Look at how happy this native cat is</em>
-`;
+        <h1>working</h1>
+        <ul>something</ul>
+        <form>
+            <h2>something</h2>
+        </form>
+      
+                    `;
 
 class PaymentPage extends Component {
 
