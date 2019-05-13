4623c3ad18581c0a4dce4605622f9692f0493559
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2019-05-13 14:44:04 +0100

    added react-native-modal

--- src/screens/PaymentPage.js
+++ src/screens/PaymentPage.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, } from 'react-native'
-import HTML from 'react-native-render-html';
+import { View, Text, Dimensions, Button } from 'react-native'
 
-const htmlContent = `
-        <h1>working</h1>
-        <ul>something</ul>
-        <form>
-            <h2>something</h2>
-        </form>
-      
-                    `;
+
 
 class PaymentPage extends Component {
 
     render() {
         return (
             <View>
-                <HTML html={htmlContent} imagesMaxWidth={Dimensions.get('window').width} />
+
             </View>
         )
     }
